6bb63a3899c1a77ff9c1327dbe32c5acc0ca1d7f
jhonatanfigueiredo <cft.figueroa@gmail.com>
2022-05-18 13:11:21 -0300

    Referenciando os posts do usuario.

--- src/main/java/com/estudos/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/estudos/workshopmongodb/config/Instantiation.java
@@ -41,5 +41,8 @@ public class Instantiation implements CommandLineRunner {
         Post post1 = new Post(null, sdf.parse("17/05/2022"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDto(maria));
         Post post2 = new Post(null, sdf.parse("18/05/2022"), "Bom dia", "Acordei feliz hoje!", new AuthorDto(maria));
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
